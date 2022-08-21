a5493abe0abcbf3446d1c5c4d7a13c8b9550870d
Jadhav Abhipray <jadhavabhipray2002@gmail.com>
2022-08-21 20:41:25 +0530

    Done

---
+++ include/home/footer.php
@@ -0,0 +1,16 @@
+﻿<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2021 Patanjali Store . All rights reserved.</p>
+					<p class="pull-right">Designed by <span>
+<a target="_blank" href="#">Sudarshan Bhavar</a></span></p>
+				</div>
+			</div>
+		</div>
+						
+    <script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
